f07eeee7f16eedd383fde754008d54a10ad8ef22
Soham0008 <patilsoham0008@gmail.com>
2024-06-21 17:20:33 +0530

    add new task through webpage

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
-from DB import getTaskList
+from flask import Flask, render_template, request, redirect, url_for
+from DB import getTaskList, addTask
 app = Flask(__name__)
 
 # TaskList=[["learn backEnd",True],["frontend Task",False],["AI/ML",False]]
-TaskList= getTaskList()
+
 @app.route("/")
-def hello_world():
+def home():
+    TaskList= getTaskList()
     return render_template("tasklist.html", tasklist =TaskList)
 
+@app.route("/add",methods=['POST'])
+def add():
+    taskname=request.form['taskName']
+    duedate= request.form['dueDate']
+    addTask(taskname,duedate)
+    return redirect(url_for('home'))
 app.run()
