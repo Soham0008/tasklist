1b593a3dc98e9cfbdbf81fff716763eecd98326d
Soham0008 <patilsoham0008@gmail.com>
2024-06-21 19:20:50 +0530

    Added add,edit & delete feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from DB import getTaskList, addTask
+from DB import getTaskList, addTask,updateTask,deleteTask
 app = Flask(__name__)
 
 # TaskList=[["learn backEnd",True],["frontend Task",False],["AI/ML",False]]
@@ -15,4 +15,16 @@ def add():
     duedate= request.form['dueDate']
     addTask(taskname,duedate)
     return redirect(url_for('home'))
+
+@app.route("/update", methods=['POST'])
+def update():
+    updatedtaskname = request.form['updateTask']
+    id = request.form['id']
+    button = request.form['saveOrDelete']
+    if button == "save":
+        updateTask(updatedtaskname,id)
+    elif button == "X":
+        deleteTask(id)
+    return redirect(url_for('home'))
+
 app.run()
