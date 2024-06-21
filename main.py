5b0462148c68138834fd687bb6cdd92cfbfd19d5
Soham0008 <patilsoham0008@gmail.com>
2024-06-21 16:07:58 +0530

    integrating flask with postgreSQL

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-
+from DB import getTaskList
 app = Flask(__name__)
 
-TaskList=[["learn backEnd",True],["frontend Task",False],["AI/ML",False]]
+# TaskList=[["learn backEnd",True],["frontend Task",False],["AI/ML",False]]
+TaskList= getTaskList()
 @app.route("/")
 def hello_world():
     return render_template("tasklist.html", tasklist =TaskList)
