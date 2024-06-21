6e1f5fbcc84f8993f1bc6fbb0ef691f3268c0bf3
Soham0008 <patilsoham0008@gmail.com>
2024-06-21 15:36:15 +0530

    version 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-TaskList=[["backEnd",True],["frontend",False],["AI/ML",False]]
+TaskList=[["learn backEnd",True],["frontend Task",False],["AI/ML",False]]
 @app.route("/")
 def hello_world():
     return render_template("tasklist.html", tasklist =TaskList)
